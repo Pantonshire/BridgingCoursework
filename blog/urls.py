158b02576c59f3699b0301b3bca4194b84cc08ec
Pantonshire <tom@tomandtally.co.uk>
2020-06-10 22:48:20 +0100

    Page for each blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog_index'),
+    path('post/<str:post_path>', views.post, name='post'),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
 from . import models
 
 def blog_index(request):
     return render(request, 'blog/index.html', {
         'posts': models.Post.objects.filter(published=True).order_by('-date')[:20],
+    })
+
+def post(request, post_path):
+    requested_post = models.Post.objects.filter(published=True, path=post_path).first()
+    if requested_post is None:
+        raise Http404('Requested post not found')
+    return render(request, 'blog/post.html', {
+        'post': requested_post,
     })
