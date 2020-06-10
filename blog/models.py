75517c009f167ba2f4e47ca214bc2dcb9fee070d
Pantonshire <tom@tomandtally.co.uk>
2020-06-10 09:57:56 +0100

    Raise max length of blog title to 255 based on information in django documentation

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     text = models.TextField()
     published = models.BooleanField(default=False, null=False)
     date = models.DateTimeField(blank=True, null=True)
