942ac6fad0f348ce52a15cd21ad61fa87d55c4ab
Pantonshire <tom@tomandtally.co.uk>
2020-06-10 10:04:52 +0100

    Explicitly disallow null values in required fields for the post

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    published = models.BooleanField(default=False, null=False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=False, null=False)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    published = models.BooleanField(default=False, blank=False, null=False)
     date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
