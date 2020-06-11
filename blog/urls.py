2811005240cb9803bdfed175cdc35ad32d0cad74
Pantonshire <tom@tomandtally.co.uk>
2020-06-11 17:12:25 +0100

    Amending posts

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/<str:post_path>', views.post, name='post'),
     path('post/<str:post_path>/amend', views.amend_post, name='amend_post'),
 
-    path('submit/post', views.submit_post, name='submit_post'),
+    path('submit/post/compose', views.submit_compose_post, name='submit_compose_post'),
+    path('submit/post/<str:post_path>/amend', views.submit_amend_post, name='submit_amend_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,7 @@ def amend_post(request, post_path):
         }),
     })
 
-def submit_post(request):
+def submit_compose_post(request):
     if request.method == "POST":
         if not request.user.has_perm('blog.add_post'):
             return HttpResponseForbidden('You do not have permission to create blog posts')
@@ -50,10 +50,32 @@ def submit_post(request):
         post.update_time()
         post.save()
         return redirect('post', post_path=post.path)
-    elif request.method == "PUT":
+    return HttpResponseBadRequest('Bad method')
+
+def submit_amend_post(request, post_path):
+    if request.method == "POST":
         if not request.user.has_perm('blog.change_post'):
             return HttpResponseForbidden('You do not have permission to edit blog posts')
-        #TODO
-        pass
-    else:
-        return HttpResponseBadRequest('Bad method')
+        requested_post = models.Post.objects.filter(path=post_path).first()
+        if requested_post is None:
+            return HttpResponseNotFound('Requested post not found')
+        form = forms.ManagePostForm(request.POST)
+        if not form.is_valid():
+            return HttpResponseBadRequest('Invalid form')
+        change_made = False
+        if requested_post.title != form.cleaned_data['title']:
+            requested_post.title = form.cleaned_data['title']
+            requested_post.update_path()
+            change_made = True
+        if requested_post.content != form.cleaned_data['content']:
+            requested_post.content = form.cleaned_data['content']
+            change_made = True
+        if requested_post.published != form.cleaned_data['published']:
+            requested_post.published = form.cleaned_data['published']
+            if requested_post.published:
+                requested_post.update_time()
+            change_made = True
+        if change_made:
+            requested_post.save()
+        return redirect('post', post_path=requested_post.path)
+    return HttpResponseBadRequest('Bad method')
